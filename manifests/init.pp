e75993144cad8a3b16bdb6cfd487b58fa90e89ed
Kris Reese <kris.reese@wwt.com>
2016-12-11 21:19:55 -0600

    Adds localhost managed host

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,12 @@ class hosts {
     purge => true,
   }
 
+  host { 'localhost':
+    ensure       => present,
+    host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
+    ip           => '127.0.0.1',
+  }
+
   @@host { $facts['fqdn']:
     ensure       => present,
     host_aliases => $facts['hostname'],
